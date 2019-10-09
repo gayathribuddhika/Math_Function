2886202615f376c086a37a34aa9ba3e60ce434ef
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-10-09 09:54:48 +0530

    Math_Function in Python
    Math_Function in Python

---
+++ Mathfuction.py
@@ -0,0 +1,35 @@
+import math
+
+x = math.sqrt(100)
+print(x)
+
+y = math.ceil(2.1)  # ceil function rounded the number to it's upper value
+print(y)
+
+z = math.floor(2.8)  # floor function rounded the number to it's lower value
+print(z)
+
+a = pow(10, 2)  # can also use 10**2.this also gives the same output
+print(a)
+
+b = math.e
+print(b)
+
+c = math.pi
+print(c)
+
+import math as m
+
+s = m.sqrt(25)
+print(s)
+
+from math import pow, sqrt
+
+print(pow(2, 3))
+print(sqrt(25))
+
+'''
+x = int(input("Enter a number : "))
+y = math.sqrt(x)
+print("Squareroot of", x, "is =", y)
+'''
